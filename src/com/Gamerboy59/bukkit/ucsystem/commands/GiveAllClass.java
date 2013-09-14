208725cc00a90d9e3031229730b9c75410b86758
Gamerboy59 <dev@united-craft.com>
2013-09-14 10:40:47 +0200

    Color Update

--- src/com/Gamerboy59/bukkit/ucsystem/commands/GiveAllClass.java
+++ src/com/Gamerboy59/bukkit/ucsystem/commands/GiveAllClass.java
@@ -1,6 +1,7 @@
 package com.Gamerboy59.bukkit.ucsystem.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,10 +20,10 @@ public class GiveAllClass implements CommandExecutor {
 		if (label.equalsIgnoreCase("giveall")) {
 			if (sender.hasPermission("crafted.giveall")) {
 				if (args.length == 0) {
-					sender.sendMessage("§cVerwendung:§b /giveall [Id] [Anzahl]");
+					sender.sendMessage(ChatColor.RED + "Verwendung: " + ChatColor.AQUA + "/giveall [Id] [Anzahl]");
 				}
 				if (args.length == 1) {
-					sender.sendMessage("§cVerwendung:§b /giveall [Id] [Anzahl]");
+					sender.sendMessage(ChatColor.RED + "Verwendung: " + ChatColor.AQUA + "/giveall [Id] [Anzahl]");
 				}
 				if (args.length == 2) {
 					int id = Integer.parseInt(args[0]);
@@ -32,11 +33,11 @@ public class GiveAllClass implements CommandExecutor {
 						try {
 						online.getInventory().addItem(new ItemStack(id, anzahl));
 						} catch (Exception e) {
-							sender.sendMessage("§cUngültige Item-Id!");
+							sender.sendMessage(ChatColor.RED + "Ungültige Item-Id!");
 							return false;
 						}
 					}
-					Bukkit.broadcastMessage("§b[§6UCS§b] §aJeder Spieler hat §6" + anzahl + " §amal §6" + id + "§a erhalten!");
+					Bukkit.broadcastMessage(ChatColor.AQUA + "[" + ChatColor.GOLD + "UCS" + ChatColor.AQUA + "] " + ChatColor.GREEN + "Jeder Spieler hat" + ChatColor.GOLD + anzahl + ChatColor.GREEN + "mal " + ChatColor.GOLD + id + ChatColor.GREEN + " erhalten!");
 				} else if (args.length == 3) {
 					int id = Integer.parseInt(args[0]);
 					int anzahl = Integer.parseInt(args[1]);
@@ -46,11 +47,11 @@ public class GiveAllClass implements CommandExecutor {
 						try {
 						online.getInventory().addItem(new ItemStack(id, anzahl, (short) olz));
 						} catch (Exception e) {
-							sender.sendMessage("§cUngültige Item-Id!");
+							sender.sendMessage(ChatColor.RED + "Ungültige Item-Id!");
 							return false;
 						}
 					}
-					Bukkit.broadcastMessage("§b[§6UCS§b] §aJeder Spieler hat §6" + anzahl + " §amal §6" + id + "§a erhalten!");
+					Bukkit.broadcastMessage(ChatColor.AQUA + "[" + ChatColor.GOLD + "UCS" + ChatColor.AQUA + "] " + ChatColor.GREEN + "Jeder Spieler hat " + ChatColor.GOLD + anzahl + ChatColor.GREEN + "mal " + ChatColor.GOLD + id + ChatColor.GREEN + " erhalten!");
 				}
 			} else {
 				sender.sendMessage(KeineRechte);
